3db6c1bb8c4700e9b5abd12a6b9a501610ca77bf
rmaWhrdl <wwscan3@gmail.com>
2023-08-02 22:59:50 +0900

    test : Add @DisplayName
    Add @DisplayName with Test Description

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,11 +1,13 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
+    @DisplayName("문자열에서 문자 변환")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
