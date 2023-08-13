7a5d8724a2fb0c487fd39858165265ec6a8b98b7
Jinhong Kim <deepred.kim@gmail.com>
2023-08-13 15:05:44 +0900

    fix: Ball 생성 시 position 유효성 검사 실패 시 던지는 예외 변경
    IllegalArgumentException -> UnknownError
    개발자의 실수로만 발생하는 예외이기 때문에 수정함

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -15,14 +15,15 @@ public class Ball{
     private void validatePosition(int position) throws IllegalArgumentException {
         boolean outOfPositionRange = position < 0 || position > 2;
         if (outOfPositionRange) {
-            throw new IllegalArgumentException("Ball의 position은 0~2 사이여야 합니다.");
+            // 개발자의 실수로만 발생하는 예외이기에 UnknownError를 던지게 하였음
+            throw new UnknownError("Ball의 position은 0~2 사이여야 합니다.");
         }
     }
 
     private void validateNumber(int number) throws IllegalArgumentException {
         boolean outOfNumberRange = number < 1 || number > 9;
         if (outOfNumberRange) {
-            throw new IllegalArgumentException("Ball의 number는 1~9 사이여야 합니다.");
+            throw new IllegalArgumentException("숫자는 1~9 사이여야 합니다.");
         }
     }
 
